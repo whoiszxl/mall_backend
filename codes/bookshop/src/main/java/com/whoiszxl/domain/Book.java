aed79b1569d08b81b0fe70f3e5e9525d4fa1ee21
whoiszxl <whoiszxl@gmail.com>
2017-12-10 12:59:51 +0800

    双向一对多

--- codes/bookshop/src/main/java/com/whoiszxl/domain/Book.java
+++ codes/bookshop/src/main/java/com/whoiszxl/domain/Book.java
@@ -1,10 +1,52 @@
 package com.whoiszxl.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 /**
  * 
  * @author zxlvoid
  *
  */
+@Entity
 public class Book {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String name;
+	
+	@ManyToOne(fetch = FetchType.LAZY) //图书对分类，为多对一,懒加载
+	private Category category;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
+		
 }
--- codes/bookshop/src/main/java/com/whoiszxl/domain/Category.java
+++ codes/bookshop/src/main/java/com/whoiszxl/domain/Category.java
@@ -3,11 +3,14 @@
  */
 package com.whoiszxl.domain;
 
+import java.util.List;
+
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -25,10 +28,13 @@ public class Category {
 
 	@Basic //字段默认basic注解
 	@Column(length = 10,nullable = false,unique = true)
-	private String username;
+	private String name;
 	
 	@Transient //不对应到数据库
 	private String xyz;
+	
+	@OneToMany(mappedBy = "category") //分类对书 一对多,放弃一对多的管理关系
+	private List<Book> books;
 
 	public Long getId() {
 		return id;
@@ -38,13 +44,31 @@ public class Category {
 		this.id = id;
 	}
 
-	public String getUsername() {
-		return username;
+	public String getName() {
+		return name;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setName(String name) {
+		this.name = name;
 	}
+
+	public String getXyz() {
+		return xyz;
+	}
+
+	public void setXyz(String xyz) {
+		this.xyz = xyz;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	
 	
 	
 	
