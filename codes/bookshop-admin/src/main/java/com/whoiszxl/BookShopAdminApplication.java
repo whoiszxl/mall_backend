bb34fb4f3cc9cde4188eada934ec072ee812a787
whoiszxl <whoiszxl@gmail.com>
2017-12-11 23:00:23 +0800

    创建restapi初始化项目

---
+++ codes/bookshop-admin/src/main/java/com/whoiszxl/BookShopAdminApplication.java
@@ -0,0 +1,15 @@
+package com.whoiszxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxlvoid
+ *
+ */
+@SpringBootApplication
+public class BookShopAdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookShopAdminApplication.class, args);
+	}
+}
---
+++ codes/bookshop-web/src/main/java/com/whoiszxl/BookShopWebApplication.java
@@ -0,0 +1,15 @@
+package com.whoiszxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxlvoid
+ *
+ */
+@SpringBootApplication
+public class BookShopWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookShopWebApplication.class, args);
+	}
+}
